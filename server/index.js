dce1e264e967563a9e106265a2fce28e21ce31ac
weaponsforge <forgeweapons@gmail.com>
2020-02-03 12:16:44 +0800

    Made email sending rate limit to 3/15 min.

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express()
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // limit each IP to 5 requests per windowMs
+  max: 3, // limit each IP to 5 requests per windowMs
   message: 'Too many requests from this IP. Please try again after 15 minutes.'
 })
 
